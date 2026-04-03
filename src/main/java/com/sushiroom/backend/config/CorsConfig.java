d2637db53789c6c11de45411316a78a6ee6c5770
roygro <princess18gro@gmail.com>
2026-04-02 19:23:14 -0600

    modificacion cors

--- src/main/java/com/sushiroom/backend/config/CorsConfig.java
+++ src/main/java/com/sushiroom/backend/config/CorsConfig.java
@@ -10,7 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:5174")
+                .allowedOrigins(
+                        "http://localhost:5173",
+                        "http://localhost:5174",
+                        "https://frontendsushiroom-production-9cb5.up.railway.app"  // ← Agrega esta línea
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
